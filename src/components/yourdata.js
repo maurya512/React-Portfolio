c3ed037e1da673e3a1e7a1ba8a7b3489817ce619
Maurya Patel <patelmaurya0512@gmail.com>
2020-10-04 03:42:08 -0400

    added source files

---
+++ src/components/yourdata.js
@@ -0,0 +1,85 @@
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+
+  //Change The Website Template
+  name: "MP",
+
+  //Contact Email
+  contactEmail: "mauryapatel12@gmail.com",
+
+  // Landing Page Name
+  landingPageName: "Maurya Patel",
+
+  // Landing Page Paragraph
+  landingPagePara:
+    "Full Stack Web Developer with an expertise in the MERN stack.",
+
+  // Landing Page Image (replace - root/public/assets/photo.jpg)
+  landingPageImage: "assets/mp1.jpg",
+
+  projects: [
+    {
+      id: 1, //DO NOT CHANGE THIS 
+      title: "Stock Watch", //Project Title - Add Your Project Title Here
+      // Image Size Should be (300X300 px)
+      imageSrc: "https://via.placeholder.com/300",
+      //Project URL - Add Your Project Url Here
+      url: "https://misterjaykay.github.io/Project-1/",
+      description: "This project was created with an incredible team to let a common working Joe get basic knowledge of the Stock Market and financial news around the world."
+    },
+    {
+      id: 2, //DO NOT CHANGE THIS
+      title: "Space Saverr",
+      imageSrc: "https://via.placeholder.com/300",
+      url: "https://github.com/ElhamFabe/Space-Saverr"
+    },
+    {
+      id: 3, //DO NOT CHANGE THIS
+      title: "Fitness Tracker(Using Remote Database)",
+      imageSrc: "https://via.placeholder.com/300",
+      url: "https://fitness-trackerr.herokuapp.com/?id=5f5da1ec7054cb0017bec71e"
+    },
+    {
+      id: 4, //DO NOT CHANGE THIS
+      title: "Online and Offline Expense Tracker",
+      imageSrc: "https://via.placeholder.com/300",
+      url: "https://agile-springs-64212.herokuapp.com/"
+    },
+    {
+      id: 5, //DO NOT CHANGE
+      title: "Coffee App(Work in progess)",
+      imageSrc: "https://via.placeholder.com/300",
+      url: "https://github.com/Carlo-Defilippis/Coffee_App"
+    }
+    // Paste more projects here from the template
+
+    /*
+                    If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
+                ,{
+                id: 7,
+                service: 'Something Amazing',
+                imageSrc: "",
+                url: ''
+            }
+                */
+  ],
+
+  // Contact Section
+  contactPara:
+    "Web Developing is more as a team. If you are looking for someone who brings great interpersonal skills as well as solid Full Stack Development knowledge let's get in touch.",
+  social: [
+    // Add Or Remove The Link Accordingly
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/maurya-patel-1696a91b2/"
+    },
+    {
+      name: "Github",
+      url: "https://www.github.com/maurya512"
+    }
+  ],
+
+  // About Page
+  aboutTitle: "I try to build something impactful for the community and am constantly evolving my skill set in order to do so!",
+  aboutPara:"A Full Stack Web Developer who graduated from an intense Coding Bootcamp eager to get involved with real world projects. I bring high energy, time punctuality and out of the box thinking to the table. I am thorough team player but can also be a self-starter. I plan to improve my current skill set with more real world projects and connecting with people highly respected in this field."
+};
