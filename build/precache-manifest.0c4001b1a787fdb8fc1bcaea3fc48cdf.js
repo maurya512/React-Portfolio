89cf770c56bd9d9e7a733b6ad0fe964751419705
Maurya Patel <patelmaurya0512@gmail.com>
2020-10-28 11:09:22 -0400

    the build for portfolio

---
+++ build/precache-manifest.0c4001b1a787fdb8fc1bcaea3fc48cdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78680504bfcc97ce69282b86df7f0a4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c7798a7f77dcbc976c8",
+    "url": "/static/css/main.d88b9176.chunk.css"
+  },
+  {
+    "revision": "37ddfaa467b1e6655d29",
+    "url": "/static/js/2.74dab61e.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.74dab61e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c7798a7f77dcbc976c8",
+    "url": "/static/js/main.c7bcfd4a.chunk.js"
+  },
+  {
+    "revision": "0ef1460328a53b041825",
+    "url": "/static/js/runtime-main.c8b6af16.js"
+  }
+]);
